afe5a90f6dc595d121a51657b3e985380f582a59
mateatomic <31723042+mateatomic@users.noreply.github.com>
2018-03-23 10:58:58 +0100

    Add files via upload

---
+++ 23.3/zad1.js
@@ -0,0 +1,20 @@
+function zadatak() {
+    var a = prompt("upisi nesto")
+    document.getElementById("b").innerText =a
+}
+
+function gumbic1(){
+    var a=parseInt(prompt("Unesi a"))
+    var b=parseInt(prompt("Unesi b"))
+
+    var pov =a*b
+    var ops =2*a+2*b
+
+    alert("pov: "+ pov+ "ops:"+ ops)
+
+
+
+
+}
+
+
---
+++ 23.3/zad2.js
@@ -0,0 +1,6 @@
+function gumbic2(){
+    var a= parseInt(document.getElementById("a").value)
+    document.getElementById("Rezultat").innerText=("Povrsina je" + a*a)
+}
+
+
